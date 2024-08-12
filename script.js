c89134f596664ebb07320b2c62694bd04c34ddac
jaogle04 <165179893+jaogle04@users.noreply.github.com>
2024-08-11 23:25:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,43 +6,57 @@ scrollWatcher.setAttribute("data-scroll-watcher", "");
 pageLinks.before(scrollWatcher);
 
 const navObserver = new IntersectionObserver((entries) => {
-  console.log(entries);
+  // console.log(entries);
   pageLinks.classList.toggle("sticking", !entries[0].isIntersecting);
 });
 
 navObserver.observe(scrollWatcher);
 
 //Top Section
-const topSection = document.querySelector("#top-section");
+// const topSection = document.querySelector("#top-section");
 
-new IntersectionObserver((entries) => {
-  console.log(entries);
-  topSection.classList.toggle(
-    "top-section-sticking",
-    !entries[0].isIntersecting
-  );
-});
+// scrollWatcher.setAttribute("data-scroll-watcher", "");
+// topSection.before(scrollWatcher);
+
+// const topSectionObserver = new IntersectionObserver((entries) => {
+//   console.log(entries);
+//   topSection.classList.toggle(
+//     "top-section-sticking",
+//     !entries[0].isIntersecting
+//   );
+// });
+
+// topSectionObserver.before(scrollWatcher);
 
 // Hamburger
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("page-links-hamburger");
   const pageLinks = document.getElementsByClassName("page-links");
-  if (!hamburger) {
-    console.error("Elements with ID 'page-links-hamburger' not found");
-    return;
-  }
-  if (!pageLinks) {
-    console.error("Elements with class 'page-links' not found");
-    return;
-  }
+  const mainContent = document.getElementsByClassName("main-content");
+
+  // if (!hamburger) {
+  //   console.error("Elements with ID 'page-links-hamburger' not found");
+  //   return;
+  // }
+  // if (!pageLinks) {
+  //   console.error("Elements with class 'page-links' not found");
+  //   return;
+  // }
   hamburger.addEventListener("click", function (clickEvent) {
     var displayClass = pageLinks[0].classList.contains("hide");
-    console.log("pageLinks", pageLinks);
-    console.log(displayClass);
+    // console.log("pageLinks", pageLinks);
+    // console.log(displayClass);
     if (displayClass === true) {
       pageLinks[0].classList.remove("hide");
+      document.body.style.overflow = "hidden";
     } else {
       pageLinks[0].classList.add("hide");
+      document.body.style.overflow = "initial";
     }
   });
 });
+
+// iOS hover
+// document.addEventListener("DOMContentLoaded", function(){
+// const =
+// })
